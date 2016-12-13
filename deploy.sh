96fd835310abb26f14e7504ca5e6097cba580955
Josh Long <josh@joshlong.com>
2016-12-12 16:40:13 -0800

    polishing names

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 mvn clean install
+an=cf-promoter
 
-cf push -p target/bg*jar
+cf push -p target/bg*jar cf-promoter --no-start
+cf set-env $an CF_ORG $CF_ORG
+cf set-env $an CF_SPACE $CF_SPACE
+cf set-env $an CF_USER $CF_USER
+cf set-env $an CF_PASSWORD $CF_PASSWORD
+cf set-env $an CF_API $CF_API
+
+cf start $an
+
